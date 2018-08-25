9e71325364bd41ed3c76b47e2722fc21748aa139
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-08-25 11:47:18 -0300

    programa virgem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleListComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
